6627e92cc709d350c660c8805e3ff12199604204
Wojciech Malota-Wojcik <wojtek@woojciki.pl>
2021-11-08 12:30:35 +0100

    Run library

---
+++ logger.go
@@ -0,0 +1,24 @@
+package run
+
+import (
+	"context"
+
+	"github.com/wojciech-malota-wojcik/logger"
+	"go.uber.org/zap"
+)
+
+var logInst *zap.Logger
+
+func log() *zap.Logger {
+	mu.Lock()
+	defer mu.Unlock()
+
+	if logInst == nil {
+		logInst = logger.New()
+	}
+	return logInst
+}
+
+func newContext() context.Context {
+	return logger.WithLogger(context.Background(), log())
+}
