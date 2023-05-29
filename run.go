d09b4b0164185614c7f3fbb45e64316b381848c1
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-05-29 21:01:35 +0200

    Add context to IoC before configuring other dependencies (#26)

--- run.go
+++ run.go
@@ -21,66 +21,64 @@ var mu sync.Mutex
 
 // Service runs service app
 func Service(appName string, containerBuilder func(c *ioc.Container), appFunc interface{}) {
-	c := ioc.New()
-	if containerBuilder != nil {
-		containerBuilder(c)
-	}
-	c.Call(run(filepath.Base(appName), logger.ServiceDefaultConfig, appFunc, parallel.Fail))
+	run(filepath.Base(appName), logger.ServiceDefaultConfig, containerBuilder, appFunc, parallel.Fail)
 }
 
 // Tool runs tool app
 func Tool(appName string, containerBuilder func(c *ioc.Container), appFunc interface{}) {
+	run(filepath.Base(appName), logger.ToolDefaultConfig, containerBuilder, appFunc, parallel.Exit)
+}
+
+func run(appName string, loggerConfig logger.Config, containerBuilder func(c *ioc.Container), setupFunc interface{}, exit parallel.OnExit) {
+	log := logger.New(logger.ConfigureWithCLI(loggerConfig))
+	if appName != "" && appName != "." {
+		log = log.Named(appName)
+	}
+	ctx := logger.WithLogger(context.Background(), log)
+
 	c := ioc.New()
+	c.Singleton(func() context.Context {
+		return ctx
+	})
 	if containerBuilder != nil {
 		containerBuilder(c)
 	}
-	c.Call(run(filepath.Base(appName), logger.ToolDefaultConfig, appFunc, parallel.Exit))
-}
 
-func run(appName string, loggerConfig logger.Config, setupFunc interface{}, exit parallel.OnExit) func(c *ioc.Container) {
-	return func(c *ioc.Container) {
-		log := logger.New(logger.ConfigureWithCLI(loggerConfig))
-		if appName != "" && appName != "." {
-			log = log.Named(appName)
-		}
-		ctx := logger.WithLogger(context.Background(), log)
+	err := parallel.Run(ctx, func(ctx context.Context, spawn parallel.SpawnFn) error {
+		spawn("", exit, func(ctx context.Context) error {
+			defer func() {
+				_ = log.Sync()
+			}()
 
-		err := parallel.Run(ctx, func(ctx context.Context, spawn parallel.SpawnFn) error {
-			spawn("", exit, func(ctx context.Context) error {
-				defer func() {
-					_ = log.Sync()
-				}()
-
-				c.Singleton(func() context.Context {
-					return ctx
-				})
-				var err error
-				c.Call(setupFunc, &err)
-				return err
+			c.Singleton(func() context.Context {
+				return ctx
 			})
-			spawn("signals", parallel.Exit, func(ctx context.Context) error {
-				sigs := make(chan os.Signal, 1)
-				signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+			var err error
+			c.Call(setupFunc, &err)
+			return err
+		})
+		spawn("signals", parallel.Exit, func(ctx context.Context) error {
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-				select {
-				case <-ctx.Done():
-					return ctx.Err()
-				case sig := <-sigs:
-					log.Info("Signal received, terminating...", zap.Stringer("signal", sig))
-				}
-				return nil
-			})
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case sig := <-sigs:
+				log.Info("Signal received, terminating...", zap.Stringer("signal", sig))
+			}
 			return nil
 		})
+		return nil
+	})
 
-		switch {
-		case err == nil:
-		case errors.Is(err, ctx.Err()):
-		case errors.Is(err, pflag.ErrHelp):
-			os.Exit(2)
-		default:
-			log.Error(fmt.Sprintf("Application returned error: %s", err), zap.Error(err))
-			os.Exit(1)
-		}
+	switch {
+	case err == nil:
+	case errors.Is(err, ctx.Err()):
+	case errors.Is(err, pflag.ErrHelp):
+		os.Exit(2)
+	default:
+		log.Error(fmt.Sprintf("Application returned error: %s", err), zap.Error(err))
+		os.Exit(1)
 	}
 }
