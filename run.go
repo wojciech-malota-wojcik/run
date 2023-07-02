52246251a01433d14293581a013b6cdd128ac6d5
Wojciech Malota-Wojcik <wojtek@exw.co>
2023-07-02 20:20:35 +0200

    New design

--- run.go
+++ run.go
@@ -14,8 +14,54 @@ import (
 	"go.uber.org/zap"
 )
 
-// Run runs the application.
-func Run(appName string, containerBuilder func(c *ioc.Container), appFunc interface{}) {
+// FlavourFunc represents a flavour function.
+type FlavourFunc func(ctx context.Context, appFunc parallel.Task) error
+
+// WithFlavours runs task with flavours installed.
+func WithFlavours(ctx context.Context, flavours []FlavourFunc, appFunc parallel.Task) error {
+	for _, f := range flavours {
+		currentAppFunc := appFunc
+		appFunc = func(ctx context.Context) error {
+			return f(ctx, currentAppFunc)
+		}
+	}
+
+	return appFunc(ctx)
+}
+
+// Environment configures environment of an application to run.
+type Environment interface {
+	WithContainerBuilder(containerBuilder func(c *ioc.Container)) Environment
+	WithFlavour(flavourFunc FlavourFunc) Environment
+	Run(appName string, appFunc interface{})
+}
+
+// New creates new environment.
+func New() Environment {
+	return &environment{}
+}
+
+var _ Environment = &environment{}
+
+type environment struct {
+	containerBuilder func(c *ioc.Container)
+	flavours         []FlavourFunc
+}
+
+// WithContainerBuilder sets IoC builder.
+func (e *environment) WithContainerBuilder(containerBuilder func(c *ioc.Container)) Environment {
+	e.containerBuilder = containerBuilder
+	return e
+}
+
+// WithFlavour adds flavour to the environment.
+func (e *environment) WithFlavour(flavourFunc FlavourFunc) Environment {
+	e.flavours = append(e.flavours, flavourFunc)
+	return e
+}
+
+// Run runs an application inside configured environment.
+func (e *environment) Run(appName string, appFunc interface{}) {
 	log := logger.New(logger.ConfigureWithCLI(logger.DefaultConfig))
 	if appName != "" && appName != "." {
 		log = log.Named(appName)
@@ -29,17 +75,19 @@ func Run(appName string, containerBuilder func(c *ioc.Container), appFunc interf
 				_ = log.Sync()
 			}()
 
-			c := ioc.New()
-			c.Singleton(func() context.Context {
-				return ctx
-			})
-			if containerBuilder != nil {
-				containerBuilder(c)
-			}
+			return WithFlavours(ctx, e.flavours, func(ctx context.Context) error {
+				c := ioc.New()
+				c.Singleton(func() context.Context {
+					return ctx
+				})
+				if e.containerBuilder != nil {
+					e.containerBuilder(c)
+				}
 
-			var err error
-			c.Call(appFunc, &err)
-			return err
+				var err error
+				c.Call(appFunc, &err)
+				return err
+			})
 		})
 		spawn("signals", parallel.Exit, func(ctx context.Context) error {
 			sigs := make(chan os.Signal, 1)
@@ -71,19 +119,4 @@ func Run(appName string, containerBuilder func(c *ioc.Container), appFunc interf
 	// e.g. when a tool starts a service. By calling os.Exit here, when started service quits,
 	// control is not passed back to the tool.
 	os.Exit(0)
-}
-
-// FlavourFunc represents a flavour function.
-type FlavourFunc func(ctx context.Context, appFunc parallel.Task) error
-
-// WithFlavours runs application with flavours installed.
-func WithFlavours(ctx context.Context, flavours []FlavourFunc, appFunc parallel.Task) error {
-	for _, f := range flavours {
-		currentAppFunc := appFunc
-		appFunc = func(ctx context.Context) error {
-			return f(ctx, currentAppFunc)
-		}
-	}
-
-	return appFunc(ctx)
 }
